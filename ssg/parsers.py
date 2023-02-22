ab610a7239641b308289de89fcc18e39e00b7704
xVergilSparda <aditya1151999@gmail.com>
2023-02-22 16:31:22 +0530

    edit

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -5,9 +5,10 @@ from pathlib import Path
 from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
-from ssg import hooks
 
 from ssg import hooks
+
+
 class Parser:
     base_ext = ".html"
     file_exts: List[str] = []
@@ -33,12 +34,13 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        self.write(path, dest, html)
         filtered = hooks.filter("generate_menu", html, self.base_ext)
         self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
 
 
 class ReStructuredTextParser(Parser):
@@ -46,7 +48,7 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
+        self.write(path, dest, html["html_body"])
         filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
         self.write(path, dest, filtered)
         sys.stdout.write(
